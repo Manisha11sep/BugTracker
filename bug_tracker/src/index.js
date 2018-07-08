b1fe9a0c5f331202013b8eb60108309f41a6ac65
Manisha11sep <er.manisha11sep@gmail.com>
2018-07-08 14:55:56 -0700

    Added ReactDOM.render()

--- bug_tracker/src/index.js
+++ bug_tracker/src/index.js
@@ -6,6 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
+
+
+// ReactDOM.render(<ComponentToRender />, targetNode)
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
